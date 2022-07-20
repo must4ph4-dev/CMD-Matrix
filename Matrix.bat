efb27e8d204b66918ad9aa6a34548a8427cf99ed
must4ph4-dev <mustafakart348@gmail.com>
2022-07-21 00:20:44 +0300

    Create Matrix.bat

---
+++ Matrix.bat
@@ -0,0 +1,8 @@
+@echo off
+pause
+color a
+mode 1000
+
+: matrix
+echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
+goto matrix
